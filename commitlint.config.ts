070f698c2baaa4e8f50edd9926f466300be3a016
Monirul Islam <mmislam027@gmail.com>
2025-11-28 14:02:19 +0600

    docs(chore): add husky and commitlint to maintain convensional commit msg

---
+++ commitlint.config.ts
@@ -0,0 +1,51 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Require a commit type
+    'type-empty': [2, 'never'],
+
+    // Require a commit scope
+    'scope-empty': [2, 'never'],
+
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'perf',
+        'build',
+        'ci',
+        'revert',
+      ],
+    ],
+
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'student',
+        'course',
+        'course-schedule',
+        'attendance',
+        'auth',
+        'user',
+        'teacher',
+        'staff',
+        'semester',
+        'config',
+        'chore',
+        'logging',
+        'security',
+        'testing',
+        'notifications',
+        'uploads',
+      ],
+    ],
+  },
+};
