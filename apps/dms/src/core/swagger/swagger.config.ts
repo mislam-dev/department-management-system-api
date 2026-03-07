03f0afe98a6dfbe6e611df4f39c58c1df5e0677e
Monirul Islam <mmislam027@gmail.com>
2026-03-08 04:03:11 +0600

    feat: setup docs for both dms and kong, show dms api along with auth service

--- apps/dms/src/core/swagger/swagger.config.ts
+++ apps/dms/src/core/swagger/swagger.config.ts
@@ -1,19 +1,17 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import * as path from 'path';
 
 export function setupSwagger(app: INestApplication): void {
-  const swaggerFile = path.join(__dirname, 'swagger.yaml');
+  const logger = new Logger('SwaggerModuleSetup');
+  const swaggerFile = path.join(__dirname, 'core/swagger', 'swagger.yaml');
   if (fs.existsSync(swaggerFile)) {
     const files = fs.readFileSync(swaggerFile, 'utf8');
     const document: OpenAPIObject = yaml.load(files) as OpenAPIObject;
-
-    SwaggerModule.setup('docs', app, document, {
-      customCssUrl: '/swagger-dark.css',
-    });
+    SwaggerModule.setup('docs', app, document);
   } else {
-    console.warn(`Swagger YAML file not found at ${swaggerFile}`);
+    logger.warn(`Swagger YAML file not found at ${swaggerFile}`);
   }
 }
